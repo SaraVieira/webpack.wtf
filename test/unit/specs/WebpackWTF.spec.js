553ee149234842a0e9d9712db01f602df151e615
Sara Vieira <hey@iamsaravieira.com>
2017-10-21 16:59:11 +0100

    fix le build

--- test/unit/specs/WebpackWTF.spec.js
+++ test/unit/specs/WebpackWTF.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import WebpackWTF from '@/components/WebpackWTF'
 
-describe('Hello.vue', () => {
+describe('WebpackWTF.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(WebpackWTF)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent).to.equal('')
     expect(vm.$el.querySelector('.link').textContent).to.equal(
