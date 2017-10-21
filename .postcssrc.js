96a0fb4666e8626f15f295fc997bf8a35bba50c0
Sara Vieira <hey@iamsaravieira.com>
2017-10-21 15:55:13 +0100

    ლ༼ ▀̿ Ĺ̯ ▀̿ ლ༽

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,7 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "smart-import": {},
-    "cssnext": {}
+  plugins: {
+    autoprefixer: {}
   }
 }
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -5,7 +5,9 @@ describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js PWA')
+    expect(vm.$el.querySelector('.hello h1').textContent).to.equal('')
+    expect(vm.$el.querySelector('.link').textContent).to.equal(
+      'WTF is Webpack ?'
+    )
   })
 })
